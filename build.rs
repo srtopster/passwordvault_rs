ecb16ba28a809236ad0b7fd54e5118a8b81ee401
Topster_ <emailcompletamenteveridico@gmail.com>
2022-11-05 21:28:27 -0300

    V0.2.0

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
-#[cfg(windows)]
-extern crate winres;
+use winres;
 
 fn main() {
-  if cfg!(target_os = "windows") {
     let mut res = winres::WindowsResource::new();
     res.set_icon("pv.ico");
     res.compile().unwrap();
-  }
 }
